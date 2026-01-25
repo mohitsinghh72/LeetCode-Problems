93fe12699b69cf9b55674088ac7dec513fb15b07
Mohit Singh <mohitsinghh72@gmail.com>
2026-01-25 23:07:38 +0530

    LeetCode Problem - 229

---
+++ Problem229.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int k = nums.length/3;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for(int i = 0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        for(int key:map.keySet()){
+            if(map.get(key) > k){
+                ans.add(key);
+            }
+        }
+        return ans;
+    }
+}
