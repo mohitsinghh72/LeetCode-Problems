c6543622d927f211e9b2927f044b383a7675ea1f
Mohit Singh <mohitsinghh72@gmail.com>
2026-02-04 00:44:54 +0530

    LeetCode Problem - 2469

---
+++ Problem2469.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double[] ans = new double[2];
+
+        double kelvin = celsius+273.15;
+
+        ans[0] = kelvin;
+
+        double fahrenheit = celsius * 1.80 + 32;
+
+        ans[1] = fahrenheit;
+
+        return ans;
+    }
+}
