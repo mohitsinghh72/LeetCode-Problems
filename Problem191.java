201444af9e54ab1c29cbd96f13dacf49c6ed5515
Mohit Singh <mohitsinghh72@gmail.com>
2026-06-13 00:47:36 +0530

    LeetCode Problem - 191

---
+++ Problem191.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        while(n>1){
+            if(n%2 == 1){
+                count++;
+            }
+            n = n>>1;
+        }
+        if(n == 1){
+            count++;
+        }
+        return count;
+    }
+}
