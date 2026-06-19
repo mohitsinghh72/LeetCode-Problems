0bbc0ff27c5e72c19465c494137b56b68731a156
Mohit Singh <mohitsinghh72@gmail.com>
2026-06-20 00:36:08 +0530

    LeetCode Problem - 39

---
+++ Problem39.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        findCombination(0,candidates,target,list,new ArrayList<>());
+        return list;
+    }
+    public void findCombination(int i,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
+        if(i == arr.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+
+        if(arr[i] <= target){
+            ds.add(arr[i]);
+            findCombination(i,arr,target-arr[i],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        findCombination(i+1,arr,target,ans,ds);
+    }
+}
