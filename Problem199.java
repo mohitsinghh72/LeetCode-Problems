017fe6a838723a609714b7e5e49e9c9421858e57
Mohit Singh <mohitsinghh72@gmail.com>
2026-08-05 00:24:58 +0530

    LeetCode Problem - 199

---
+++ Problem199.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        rightSideView(root,0,list);
+        return list;
+    }
+    public void rightSideView(TreeNode root,int level,List<Integer> list){
+        if(root == null){
+            return;
+        }
+        if(level == list.size()){
+            list.add(root.val);
+        }
+        rightSideView(root.right,level+1,list);
+        rightSideView(root.left,level+1,list);
+    }
+}
